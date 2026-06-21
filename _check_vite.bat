640127dbf2df9543dc32caf00c9cbf7fb4071571
PilotDesk Developer <dev@pilotdesk.local>
2026-06-22 01:00:18 +0800

    chore: cleanup temp files and update project config

--- _check_vite.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /d "E:\WorkSpace_HermesAgent\pilotdeskProject\pilotdesk"
-call npx.cmd vite build > "E:\WorkSpace_HermesAgent\pilotdeskProject\pilotdesk\_vite_output.txt" 2>&1
-echo EXIT_CODE=%errorlevel%
